fa9357e4f76ffd7e3cc4e98679686c94985f1bf0
ilhan2021 <ilhanilhan.2021@gmail.com>
2022-10-31 23:41:14 +0300

    backend project

---
+++ visionrent/src/main/java/com/visionrent/controller/ContactMessageController.java
@@ -0,0 +1,19 @@
+package com.visionrent.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contactmessage")
+public class ContactMessageController {
+	
+	private ContactMessageController contactMessageController;
+	
+	@Autowired
+	public ContactMessageController(ContactMessageController contactMessageController) {
+		
+		this.contactMessageController = contactMessageController;
+	}
+
+}
