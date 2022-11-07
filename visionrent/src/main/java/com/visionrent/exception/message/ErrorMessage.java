5bbd14cac4f3f21e13e681531913f849956fed3c
ilhan2021 <ilhanilhan.2021@gmail.com>
2022-11-08 01:26:27 +0300

    visionrent-project

--- visionrent/src/main/java/com/visionrent/exception/message/ErrorMessage.java
+++ visionrent/src/main/java/com/visionrent/exception/message/ErrorMessage.java
@@ -4,9 +4,15 @@ public class ErrorMessage {
 	public final static String RESOURCE_NOT_FOUND_MESSAGE = "Resource with id %d not found";
 	public final static String ROLE_NOT_FOUND_MESSAGE = "Role: %s not found";
 	public final static String USER_NOT_FOUND_MESSAGE = "User with Email %s not found";
+	public final static String PRINCIPAL_FOUND_MESSAGE = "User not found";
+	
 	
 	
 	public final static String JWTTOKEN_ERROR_MESSAGE = "JWT Token Validation Error: %s";
 	
 	public final static String EMAIL_ALREADY_EXIST_MESSAGE = "Email: %s already exist";
+
+	
+	
+	
 }
--- visionrent/src/main/java/com/visionrent/service/UserService.java
+++ visionrent/src/main/java/com/visionrent/service/UserService.java
@@ -2,41 +2,51 @@ package com.visionrent.service;
 
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import javax.validation.Valid;
+import java.util.function.Function;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.visionrent.domain.Role;
 import com.visionrent.domain.User;
 import com.visionrent.domain.enums.RoleType;
+import com.visionrent.dto.UserDTO;
 import com.visionrent.dto.request.RegisterRequest;
 import com.visionrent.exception.ConflictException;
 import com.visionrent.exception.ResourceNotFoundException;
 import com.visionrent.exception.message.ErrorMessage;
+import com.visionrent.mapper.UserMapper;
 import com.visionrent.repository.UserRepository;
+import com.visionrent.security.SecurityUtils;
 @Service
 public class UserService {
 	
-
+	
 	private UserRepository userRepository;
 	
-
+	
 	private RoleService roleService ;
 	
-
 	private PasswordEncoder passwordEncoder;
+	
+	private UserMapper userMapper;
 	
 	
 	
 	@Autowired
-	public UserService(UserRepository userRepository, RoleService roleService, @Lazy PasswordEncoder passwordEncoder) {
+	public UserService(UserRepository userRepository, RoleService roleService, @Lazy PasswordEncoder passwordEncoder, UserMapper userMapper) {
 		super();
 		this.userRepository = userRepository;
 		this.roleService = roleService;
 		this.passwordEncoder = passwordEncoder;
+		this.userMapper = userMapper;
 	}
+	
 	public User getUserByEmail(String email ) {
 		
 		  User user  =  userRepository.findByEmail(email).orElseThrow(()->
@@ -44,6 +54,7 @@ public class UserService {
 				);
 		return user ;
 	}
+	
 	public void saveUser(RegisterRequest registerRequest) {
 		if(userRepository.existsByEmail(registerRequest.getEmail())) {
 			throw new ConflictException(String.format(ErrorMessage.EMAIL_ALREADY_EXIST_MESSAGE,
@@ -73,5 +84,58 @@ public class UserService {
 		
 		
 	}
+	
+	public List<UserDTO> getAllUsers() {
+		
+		    List<User> users = userRepository.findAll();
+		     List<UserDTO> userDTOs = userMapper.map(users);
+		    
+		     return userDTOs;
+		
+	}
+	
+	public UserDTO getPrincipal() {
+		 User currentUser =  getCurrentUser();
+		  // return userMapper.userToUserDTO(currentUser);
+		  UserDTO userDTO = userMapper.userToUserDTO(currentUser);
+		  return userDTO;
+	
+		
+	}
+	
+	public User getCurrentUser() {
+		String email = SecurityUtils.getCurrentUserLogin().orElseThrow(()->
+		 new ResourceNotFoundException(ErrorMessage.PRINCIPAL_FOUND_MESSAGE));
+		User user =  getUserByEmail(email);
+		return user ;
+		
+	}
+	
+	public Page<UserDTO> getUserPage(Pageable pageable) {
+		   Page<User> userPage = userRepository.findAll(pageable);
+		  
+	        return getUserDTOPage(userPage);
+	
+	}
+	private Page<UserDTO> getUserDTOPage(Page<User> userPage) {
+		
+		 Page<UserDTO> userDTOPage =  userPage.map(new Function<User, UserDTO>() {
+			 @Override
+			public UserDTO apply(User user) {
+				
+				return userMapper.userToUserDTO(user);
+			}
+		});
+		
+		 return userDTOPage;
+		
+	}
+	public UserDTO getUserById(Long id) {
+	    User user = userRepository.findById(id).orElseThrow(()->
+	  
+			   new ResourceNotFoundException(String.format(ErrorMessage.RESOURCE_NOT_FOUND_MESSAGE, id)));
+	    return userMapper.userToUserDTO(user);
+		}
+
 	
 }
